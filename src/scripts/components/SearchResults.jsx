fe0a60e1d62823d569da9006f0cb96e5f45790dc
Jason L Perry <jasper@ambethia.com>
2015-10-19 11:43:31 -0400

    Models

--- src/scripts/components/SearchResults.jsx
+++ src/scripts/components/SearchResults.jsx
@@ -1,34 +1,58 @@
 import React from 'react';
+import Icon from './Icon';
+import SearchDetail from './SearchDetail';
 
 class SearchResults extends React.Component {
 
-  render () {
-    return <div className="container">
-    <div className="results">
-      <h1>Search Results</h1>
-      </div>
-      <div className="well inline">
-      <p className="wellp">👥 2-4</p>
-      <p className="wellp">⏳45</p>
-      <p className="wellp">Difficulty: <button className="btn-success">Easy</button></p>
-     </div>
-      <div className="panel panel-default">
-      <div className="panel-heading">
-       <h3 className="panel-title">Game Title</h3>
-      </div>
-      <div className="panel-body">
-       <img className="gameImg" src="https://www.skylib.com/img/game_placeholder.jpg" />
-       <p><i>Game Description here..</i></p>
-       <div className="inline">
-       <p>👥 3 </p>
-       <p>⏳45 </p>
-       <p><button className="btn-success">Easy</button></p>
-       <p>⭐️⭐️⭐️⭐️</p>
-       </div>
-    </div>
-  </div>
-</div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      models: props.collection.models
+    };
+  }
 
+  componentWillMount() {
+    this.props.collection.on('sync update', this.handleSync);
+    this.props
+      .collection
+      .fetch();
+  }
+
+  handleSync = () => {
+    this.setState({
+      models: this.props.collection.models
+    });
+  }
+
+  render () {
+    let details = this.state
+      .models
+      .map((model, i) => {
+        return <SearchDetail key={i} model={model}/>;
+      });
+
+    return (
+      <div className="container">
+        <div className="results">
+          <h1>Search Results</h1>
+        </div>
+        <div className="well inline">
+          <p className="wellp">
+            <Icon type="users"/>
+            2-4
+          </p>
+          <p className="wellp">
+            <Icon type="hourglass-half"/>
+            45
+          </p>
+          <p className="wellp">
+            <Icon type="cogs"/>
+            Easy
+          </p>
+        </div>
+        {details}
+      </div>
+    );
   }
 }
 
